b23d09d19320577a4ad871cbf4c47671f9ea3f22
Alexander Naumov <naumov@tozen.de>
2015-10-13 11:41:03 +0200

    add contao 4.0 support

--- config/autoload.php
+++ config/autoload.php
@@ -2,25 +2,29 @@
 
 \Contao\ClassLoader::addNamespace('FModule');
 
+
+$pathToFiles = version_compare(VERSION, '4.0', '>=') ? 'vendor/fmodule/fmodule/' : 'system/modules/fmodule/';
+
+
 /**
  * Register the classes
  */
 ClassLoader::addClasses(array
 (
-    'FModule\DCACreator' => 'system/modules/fmodule/src/Resources/contao/classes/DCACreator.php',
-    'FModule\DCAModuleSettings' => 'system/modules/fmodule/src/Resources/contao/classes/DCAModuleSettings.php',
-    'FModule\DCAModuleData' => 'system/modules/fmodule/src/Resources/contao/classes/DCAModuleData.php',
-    'FModule\SqlData' => 'system/modules/fmodule/src/Resources/contao/classes/SqlData.php',
-    'FModule\OptionWizardExtended' => 'system/modules/fmodule/src/Resources/contao/widget/OptionWizardExtended.php',
-    'FModule\DCAHelper' => 'system/modules/fmodule/src/Resources/contao/classes/DCAHelper.php',
-    'FModule\ModuleDetailView' => 'system/modules/fmodule/srcResources/contao/module/ModuleDetailView.php',
-    'FModule\ModuleListView' => 'system/modules/fmodule/src/Resources/contao/module/ModuleListView.php',
-    'FModule\ModeSettings' => 'system/modules/fmodule/src/Resources/contao/widget/ModeSettings.php',
-    'FModule\FilterFields' => 'system/modules/fmodule/src/Resources/contao/widget/FilterFields.php',
-    'FModule\ModuleFormFilter' => 'system/modules/fmodule/src/Resources/contao/module/ModuleFormFilter.php',
-    'FModule\FieldAppearance' => 'system/modules/fmodule/src/Resources/contao/classes/FieldAppearance.php',
-    'FModule\ContentModelExtend' => 'system/modules/fmodule/src/Resources/contao/model/ContentModelExtend.php',
-    'FModule\FModule' => 'system/modules/fmodule/src/Resources/contao/classes/FModule.php',
+    'FModule\DCACreator' => $pathToFiles.'src/Resources/contao/classes/DCACreator.php',
+    'FModule\DCAModuleSettings' => $pathToFiles.'src/Resources/contao/classes/DCAModuleSettings.php',
+    'FModule\DCAModuleData' => $pathToFiles.'src/Resources/contao/classes/DCAModuleData.php',
+    'FModule\SqlData' => $pathToFiles.'src/Resources/contao/classes/SqlData.php',
+    'FModule\OptionWizardExtended' => $pathToFiles.'src/Resources/contao/widget/OptionWizardExtended.php',
+    'FModule\DCAHelper' => $pathToFiles.'src/Resources/contao/classes/DCAHelper.php',
+    'FModule\ModuleDetailView' => $pathToFiles.'src/Resources/contao/module/ModuleDetailView.php',
+    'FModule\ModuleListView' => $pathToFiles.'src/Resources/contao/module/ModuleListView.php',
+    'FModule\ModeSettings' => $pathToFiles.'src/Resources/contao/widget/ModeSettings.php',
+    'FModule\FilterFields' => $pathToFiles.'src/Resources/contao/widget/FilterFields.php',
+    'FModule\ModuleFormFilter' => $pathToFiles.'src/Resources/contao/module/ModuleFormFilter.php',
+    'FModule\FieldAppearance' => $pathToFiles.'src/Resources/contao/classes/FieldAppearance.php',
+    'FModule\ContentModelExtend' => $pathToFiles.'src/Resources/contao/model/ContentModelExtend.php',
+    'FModule\FModule' => $pathToFiles.'src/Resources/contao/classes/FModule.php',
 
 ));
 
@@ -29,16 +33,16 @@ ClassLoader::addClasses(array
  */
 TemplateLoader::addFiles(array
 (
-	'mod_fmodule_detail' => 'system/modules/fmodule/templates',
-	'mod_fmodule_list' => 'system/modules/fmodule/templates',
-    'fm_form_filter' => 'system/modules/fmodule/templates/form',
-    'fmodule_full' => 'system/modules/fmodule/templates/list',
-    'fmodule_teaser' => 'system/modules/fmodule/templates/list',
-    'mod_form_filter' => 'system/modules/fmodule/templates',
+	'mod_fmodule_detail' => $pathToFiles.'src/Resources/contao/templates',
+	'mod_fmodule_list' => $pathToFiles.'src/Resources/contao/templates',
+    'fm_form_filter' => $pathToFiles.'src/Resources/contao/templates/form',
+    'fmodule_full' => $pathToFiles.'src/Resources/contao/templates/list',
+    'fmodule_teaser' => $pathToFiles.'src/Resources/contao/templates/list',
+    'mod_form_filter' => $pathToFiles.'src/Resources/contao/templates',
 
-    'fm_widget_date_field' => 'system/modules/fmodule/templates/widgets',
-    'fm_widget_multi_choice' => 'system/modules/fmodule/templates/widgets',
-    'fm_widget_simple_choice' => 'system/modules/fmodule/templates/widgets',
-    'fm_widget_search_field' => 'system/modules/fmodule/templates/widgets'
+    'fm_widget_date_field' => $pathToFiles.'src/Resources/contao/templates/widgets',
+    'fm_widget_multi_choice' => $pathToFiles.'src/Resources/contao/templates/widgets',
+    'fm_widget_simple_choice' => $pathToFiles.'src/Resources/contao/templates/widgets',
+    'fm_widget_search_field' => $pathToFiles.'src/Resources/contao/templates/widgets'
 
 ));

cc618565968b3691a39a31ff57e4187bfdd07b5b
Alexander Naumov <naumov@tozen.de>
2015-10-15 14:56:56 +0200

    fix typo

--- languages/en/tl_fmodules_license.php
+++ languages/en/tl_fmodules_license.php
@@ -14,7 +14,7 @@
 $GLOBALS['TL_LANG']['tl_fmodules_license']['license_legend'] = 'License';
 
 
-$GLOBALS['TL_LANG']['tl_fmodules_license']['fmodule_license'][0] = 'license';
+$GLOBALS['TL_LANG']['tl_fmodules_license']['fmodule_license'][0] = 'License';
 $GLOBALS['TL_LANG']['tl_fmodules_license']['fmodule_license'][1] = 'Please enter valid license key';
 
 $GLOBALS['TL_LANG']['tl_fmodules_license']['invalidKey'] = 'Your license key is invalid.';

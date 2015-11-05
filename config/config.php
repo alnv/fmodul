f0e06d40f64c4d09c5eb16bba6b6ebfc7bbf1a44
Alexander Naumov <alex_naumov@web.de>
2015-11-05 12:56:27 +0100

    add trail_fmodule meta tag and remove div

--- config/config.php
+++ config/config.php
@@ -78,7 +78,7 @@ if (TL_MODE == 'FE') {
     $validSums = new \FModule\FModule();
     $license = \Contao\Config::get('fmodule_license');
     if (!isset($license) || !in_array(md5($license), $validSums->validSums, true)) {
-        $GLOBALS['TL_HEAD'][] = '<div style="position:fixed; bottom: 0; left: 0; width: 100%; background: tomato; padding-top: 10px; padding-bottom: 10px; font-size: 14px; color: #fff; z-index: 9999; text-align: center;">F Modul Lizenz <a href="http://fmodul.alexandernaumov.de/kaufen.html" target="_blank" style="color: #fff; text-decoration: underline; font-weight: 600;">kaufen</a></div>';
+	    $GLOBALS['TL_HEAD'][] = '<meta name="trail_fmodule" content="http://fmodul.alexandernaumov.de/kaufen.html" />';
     }
 
 }

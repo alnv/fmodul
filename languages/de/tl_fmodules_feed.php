c90b27ea652f1cec6244d5356c5f63d5dff9fffa
Alexander Naumov <naumov@tozen.de>
2015-11-25 15:35:20 +0100

    add labels for fmodules_feeds table

---
+++ languages/de/tl_fmodules_feed.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   F Modul
+ * @author    Alexander Naumov http://www.alexandernaumov.de
+ * @license   commercial
+ * @copyright 2015 Alexander Naumov
+ */
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['title_legend'] = 'Titel und Sprache';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['wrappers_legend'] = 'Wrapper';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['config_legend'] = 'Feed-Einstellungen';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['new'][0] = 'Feed anlegen';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['new'][1] = 'Feed anlegen';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['editheader'][0] = 'Feed bearbeiten';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['editheader'][1] = 'Feed bearbeiten';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['delete'][0] = 'Feed löschen';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['delete'][1] = 'Feed löschen';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['copy'][0] = 'Feed Kopieren';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['copy'][1] = 'Feed Kopieren';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['show'][0] = 'Feed Information';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['show'][1] = 'Feed Information';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['title'][0] = 'Titel';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['title'][1] = 'Bitte geben Sie einen Feed-Titel ein.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['alias'][0] = 'Feed-Alias';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['alias'][1] = 'Hier können Sie einen eindeutigen Dateinamen (ohne Endung) eingeben. Die XML-Datei wird automatisch im share-Ordner Ihrer Contao-Installation erstellt.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['language'][0] = 'Feed-Sprache';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['language'][1] = 'Bitte geben Sie die Sprache der Seite gemäß des ISO-639 Standards ein (z.B. de, de-ch).';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['fmodule'][0] = 'F Modul auswählen';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['fmodule'][1] = 'Wählen Sie einen F Modul aus.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['wrappers'][0] = 'Wrapper auswählen';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['wrappers'][1] = 'Hier legen Sie fest, welche Wrapper in dem Feed enthalten sind.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['format'][0] = 'Feed-Format';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['format'][1] = 'Bitte wählen Sie ein Format.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['source'][0] = 'Export-Einstellungen';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['source'][1] = 'Hier können Sie festlegen, was exportiert werden soll.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['maxItems'][0] = 'Maximale Anzahl an Beiträgen';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['maxItems'][1] = 'Hier können Sie die Anzahl der Beiträge limitieren. Geben Sie 0 ein, um alle zu exportieren.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['feedBase'][0] = 'Basis-URL';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['feedBase'][1] = 'Bitte geben Sie die Basis-URL mit Protokoll (z.B. http://) ein.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['description'][0] = 'Feed-Beschreibung';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['description'][1] = 'Bitte geben Sie eine kurze Beschreibung des Wrapper-Feeds ein.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['source_teaser'] = 'Teasertexte';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['source_text'] = 'Komplette Beiträge';
---
+++ languages/en/tl_fmodules_feed.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   F Modul
+ * @author    Alexander Naumov http://www.alexandernaumov.de
+ * @license   commercial
+ * @copyright 2015 Alexander Naumov
+ */
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['title_legend'] = 'Title and language';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['wrappers_legend'] = 'Wrapper';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['config_legend'] = 'Feed settings';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['new'][0] = 'New feed';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['new'][1] = 'New feed';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['editheader'][0] = 'Edit feed';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['editheader'][1] = 'Edit feed';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['delete'][0] = 'Delete feed';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['delete'][1] = 'Delete feed';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['copy'][0] = 'Copy feed';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['copy'][1] = 'Copy feed';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['show'][0] = 'Show Information';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['show'][1] = 'Show Information';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['title'][0] = 'Title';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['title'][1] = 'Please enter a feed title.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['alias'][0] = 'Feed alias';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['alias'][1] = 'Here you can enter a unique filename (without extension). The XML feed file will be auto-generated in the share directory of your Contao installation.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['fmodule'][0] = 'Select F Module';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['fmodule'][1] = 'Select F Module.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['language'][0] = 'Feed language';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['language'][1] = 'Please enter the feed language according to the ISO-639 standard (e.g. en or en-us).';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['wrappers'][0] = 'Wrappers';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['wrappers'][1] = 'Here you can choose the FModule wrappers to be included in the feed.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['format'][0] = 'Feed-Format';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['format'][1] = 'Please choose a feed format.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['source'][0] = 'Export settings';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['source'][1] = 'Here you can choose what will be exported.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['maxItems'][0] = 'Maximum number of items';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['maxItems'][1] = 'Here you can limit the number of feed items. Set to 0 to export all.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['feedBase'][0] = 'Base URL';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['feedBase'][1] = 'Please enter the base URL with protocol (e.g. http://).';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['description'][0] = 'Feed description';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['description'][1] = 'Here you can enter a short description of the news feed.';
+
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['source_teaser'] = 'Teaser';
+$GLOBALS['TL_LANG']['tl_fmodules_feed']['source_text'] = 'Full articles';

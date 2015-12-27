391cd19bb0804b378bcd4928bda289e7c140805f
Alexander Naumov <alex_naumov@web.de>
2015-12-27 07:46:38 +0100

    add shortcut labels

---
+++ languages/de/tl_prosearch_data.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['ff'] = 'F Modul - Filter';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['fm'] = 'F Modul';
---
+++ languages/en/tl_prosearch_data.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['ff'] = 'F Module - Filters';
+$GLOBALS['TL_LANG']['tl_prosearch_data']['shortcut']['fm'] = 'F Module';

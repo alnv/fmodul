2a7526bce8d6ee7707a042fb6ae4ca1b8753bd38
Alexander Naumov <alex_naumov@web.de>
2015-12-27 07:47:08 +0100

    add ProSearchApi

---
+++ src/Resources/contao/classes/ProSearchApi.php
@@ -0,0 +1,56 @@
+<?php namespace FModule;
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   F Modul
+ * @author    Alexander Naumov http://www.alexandernaumov.de
+ * @license   commercial
+ * @copyright 2015 Alexander Naumov
+ */
+
+class ProSearchApi
+{
+	
+	/**
+	 *
+	 */
+	public function setCustomIcon($table, $db, $dataArr, $dca)
+	{
+		$iconName = '';
+		
+		if($table == 'tl_fmodules')
+        {
+            $iconName = $GLOBALS['FM_AUTO_PATH'].'icon.png';
+        }
+        
+        if($table == 'tl_fmodules_filters')
+        {
+            $iconName = $GLOBALS['FM_AUTO_PATH'].'filter.png';
+        }
+        
+        return $iconName;
+	}
+	
+	/**
+	 *
+	 */
+	public function setCustomShortcut($table, $db, $dataArr, $dca)
+	{
+		$shortcut = '';
+
+        if($table == 'tl_fmodules')
+        {
+            $shortcut = 'fm';
+        }
+        
+        if($table == 'tl_fmodules_filters')
+        {
+            $shortcut = 'ff';
+        }
+        return $shortcut;
+	} 	
+	
+}

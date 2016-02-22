0c54708c16f90a84be94495ea1e887d573aefdc8
Alexander Naumov <naumov@tozen.de>
2016-02-22 16:21:34 +0100

    add ignore option to toggle field

--- src/Resources/contao/widget/ModeSettings.php
+++ src/Resources/contao/widget/ModeSettings.php
@@ -13,30 +13,48 @@
 
 use Contao\Widget;
 
+/**
+ * Class ModeSettings
+ * @package FModule
+ */
 class ModeSettings extends Widget
 {
 
+    /**
+     * @var bool
+     */
     protected $blnSubmitInput = true;
 
+    /**
+     * @var string
+     */
     protected $strTemplate = 'be_widget';
 
+    /**
+     * @var array
+     */
     private $modeViewObject = array();
 
+    /**
+     *
+     */
     public function validate()
     {
         $this->varValue = serialize($this->getPost($this->strName));
     }
 
+    /**
+     * @return string
+     */
     public function generate()
     {
 
         $allowedDCA = array('tl_module', 'tl_page');
-        $doNotSetByType = array('wrapper_field', 'legend_start', 'legend_end', 'widget');
-        $doNotSetByID = array('orderBy', 'sorting_fields', 'pagination');
+        $doNotSetByType = array('wrapper_field', 'legend_start', 'legend_end', 'widget', 'fulltext_search');
+        $doNotSetByID = array('orderBy', 'sorting_fields', 'pagination', 'auto_item', 'auto_page');
 
-        if(!in_array($this->strTable, $allowedDCA))
-        {
-            return 'Taxonomy field is not allowed to used in '.$this->strTable;
+        if (!in_array($this->strTable, $allowedDCA)) {
+            return 'Taxonomy field is not allowed to used in ' . $this->strTable;
         }
 
         // contao
@@ -47,15 +65,14 @@ class ModeSettings extends Widget
         $this->import('Database');
 
         $fmoduleDB = null;
-        if($this->strTable == 'tl_module')
-        {
+        if ($this->strTable == 'tl_module') {
             $fmoduleDB = $this->Database->prepare("SELECT f_select_module, f_select_wrapper FROM tl_module WHERE id = ?")->execute($this->currentRecord)->row();
         }
 
         $modulename = $fmoduleDB ? $fmoduleDB['f_select_module'] : '';
         $wrapperID = $fmoduleDB ? $fmoduleDB['f_select_wrapper'] : '';
 
-        if ($this->strTable == 'tl_module' && ( $modulename == '' || $wrapperID == '' ) ) {
+        if ($this->strTable == 'tl_module' && ($modulename == '' || $wrapperID == '')) {
             return '<p>Please select Backend Modul</p>';
         }
 
@@ -66,34 +83,30 @@ class ModeSettings extends Widget
         $modeSettingsDB = null;
         $optionsDB = null;
         //
-        if($this->strTable == 'tl_module')
-        {
+        if ($this->strTable == 'tl_module') {
             $modeSettingsDB = $this->Database->prepare('SELECT tl_fmodules.tablename, tl_fmodules.id AS fmoduleID, tl_fmodules_filters.* FROM tl_fmodules JOIN tl_fmodules_filters ON tl_fmodules.id = tl_fmodules_filters.pid WHERE tablename = ? ORDER BY sorting')->execute($modulename);
             $optionsDB = $this->Database->prepare('SELECT * FROM ' . $modulename . ' WHERE id = ?')->execute($wrapperID)->row();
         }
 
         //
-        if($this->strTable == 'tl_page')
-        {
+        if ($this->strTable == 'tl_page') {
             $modeSettingsDB = $this->Database->prepare('SELECT tl_fmodules.tablename, tl_fmodules.id AS fmoduleID, tl_fmodules_filters.* FROM tl_fmodules JOIN tl_fmodules_filters ON tl_fmodules.id = tl_fmodules_filters.pid ORDER BY sorting')->execute();
         }
 
         //
-        if($optionsDB == null && $this->strTable == 'tl_page')
-        {
+        if ($optionsDB == null && $this->strTable == 'tl_page') {
             $options = array();
 
-            while($modeSettingsDB->next())
-            {
-                if ( in_array($modeSettingsDB->fieldID, $doNotSetByID) ) {
+            while ($modeSettingsDB->next()) {
+                if (in_array($modeSettingsDB->fieldID, $doNotSetByID)) {
                     continue;
                 }
 
-                if ( in_array($modeSettingsDB->type, $doNotSetByType) ) {
+                if (in_array($modeSettingsDB->type, $doNotSetByType)) {
                     continue;
                 }
 
-                $options[$modeSettingsDB->fieldID] = $this->Database->prepare('SELECT '.$modeSettingsDB->fieldID.' FROM ' . $modeSettingsDB->tablename . '')->execute()->row()[$modeSettingsDB->fieldID];
+                $options[$modeSettingsDB->fieldID] = $this->Database->prepare('SELECT ' . $modeSettingsDB->fieldID . ' FROM ' . $modeSettingsDB->tablename . '')->execute()->row()[$modeSettingsDB->fieldID];
             }
 
             $optionsDB = $options;
@@ -101,17 +114,23 @@ class ModeSettings extends Widget
 
         }
 
-        $index = 0;
-        $input = $this->varValue;
+
+        $savedValues = $this->varValue;
 
         $defaultSet = array(
             'filterValue' => '',
             'overwrite' => '0'
         );
 
-        if($modeSettingsDB->count() < 1)
+        if ($modeSettingsDB->count() < 1) {
+            return 'no fields found';
+        }
+
+        $input = array();
+
+        foreach($savedValues as $fid => $savedValue)
         {
-            return 'No fields found';
+            $input[$fid] = $savedValue;
         }
 
         while ($modeSettingsDB->next()) {
@@ -127,7 +146,7 @@ class ModeSettings extends Widget
             $options = $optionsDB[$modeSettingsDB->fieldID];
 
             $viewObject = array(
-                "active" => ($input[$index]['active'] ? '1' : '0'),
+                "active" => ($input[$modeSettingsDB->fieldID]['active'] ? '1' : '0'),
                 "fieldID" => $modeSettingsDB->fieldID,
                 "type" => $modeSettingsDB->type,
                 "title" => $modeSettingsDB->title,
@@ -138,11 +157,11 @@ class ModeSettings extends Widget
                 "isInteger" => $modeSettingsDB->isInteger,
                 "addTime" => $modeSettingsDB->addTime,
                 "options" => (!deserialize($options) ? array() : deserialize($options)),
-                "set" => ($input[$index]['set'] ? $input[$index]['set'] : $defaultSet)
+                "set" => ($input[$modeSettingsDB->fieldID]['set'] ? $input[$modeSettingsDB->fieldID]['set'] : $defaultSet)
             );
 
             $this->modeViewObject[] = $viewObject;
-            $index++;
+
         }
 
         $return =
@@ -158,10 +177,14 @@ class ModeSettings extends Widget
 
     }
 
-
+    /**
+     * @return string
+     */
     private function setModeBlocks()
     {
         $html = '<div class="fmode_settings">';
+
+        //$index = 0;
 
         $methods = array(
             'simple_choice' => 'setSimpleChoiceSettings',
@@ -171,18 +194,18 @@ class ModeSettings extends Widget
             'toggle_field' => 'setToggleFieldSettings'
         );
 
-        foreach ($this->modeViewObject as $index => $viewObject) {
+        foreach ($this->modeViewObject as $viewObject) {
 
-            if ($viewObject['type'] == 'fulltext_search') {
-                continue;
-            }
+            //if ($viewObject['type'] == 'fulltext_search') {
+            //    continue;
+            //}
 
             $str = '<div class="f_checkbox">
                        <h4><input type="checkbox" value="1" name="%s" id="%s" %s %s> <label for="%s">%s</label></h4>
-                       <p class="tl_help tl_tip" title="">' . $GLOBALS['TL_LANG']['MSC']['fm_activate_filter'] . '</p>
+                       <p class="tl_help tl_tip" title="">' . sprintf($GLOBALS['TL_LANG']['MSC']['fm_activate_filter'], $viewObject['title'], $viewObject['fieldID']) . '</p>
                     </div>';
 
-            $name = $this->strName . '[' . $index . '][active]';
+            $name = $this->strName . '[' . $viewObject['fieldID'] . '][active]';
             $id = "ctrl_" . $viewObject['fieldID'];
             $checked = ($viewObject['active'] == '1' ? 'checked="checked"' : '');
             $attributes = $this->getAttributes();
@@ -196,17 +219,23 @@ class ModeSettings extends Widget
             if ($viewObject['active'] == '1') {
 
                 $func = $methods[$viewObject['type']];
-                $temp = call_user_func(array($this, $func), $index, $viewObject);
+                $temp = call_user_func(array($this, $func), $viewObject['fieldID'], $viewObject);
                 $box = '<div class="f_settings">' . $temp . '</div>';;
                 $html = $html . $box;
             }
+
+            //$index++;
 
         }
 
         return $html . '</div>';
     }
-	
-	
+
+    /**
+     * @param $index
+     * @param $viewObject
+     * @return string
+     */
 	private function setToggleFieldSettings($index, $viewObject)
 	{
 		
@@ -215,29 +244,37 @@ class ModeSettings extends Widget
 		
 		$label1 = $GLOBALS['TL_LANG']['MSC']['fm_highlight_show'];
 		$label2 = $GLOBALS['TL_LANG']['MSC']['fm_highlight_ignore'];
-		
 		$template = 
 			'<div>
 				<div>
 					<input type="hidden" value="'.$viewObject['fieldID'].'" name="' . $this->strName . '[' . $index . '][fieldID]">
 					<h4><label>'.$GLOBALS['TL_LANG']['MSC']['fm_highlight'].'</label></h4>
 					<select class="tl_select" value="' . $viewObject['set']['filterValue'] . '" name="' . $this->strName . '[' . $index . '][set][filterValue]">
-                        <option value="1" ' . ($selected ? 'selected' : '') . '>'.$label1.'</option>
+					   <option value="1" ' . ($selected ? 'selected' : '') . '>'.$label1.'</option>
                         <option value="" ' . (!$selected ? 'selected' : '') . '>'.$label2.'</option>
                     </select>
                     <p class="tl_help tl_tip" title="">' . $desc . '</p>
 				</div>
 				<div>
+				    <div class="mode_checkbox">
+                        <h4><input type="checkbox" id="ctrl_' . $index . '[' . $viewObject['fieldID'] . '][ignore]" value="1" name="' . $this->strName . '[' . $index . '][set][ignore]" ' . ($viewObject['set']['ignore'] == '1' ? 'checked="checked"' : '') . '><label for="ctrl_' . $index . '[' . $viewObject['fieldID'] . '][ignore]">'.$GLOBALS['TL_LANG']['MSC']['fm_field_ignore'].'</label></h4>
+                        <p class="tl_help tl_tip" title="">'.$GLOBALS['TL_LANG']['MSC']['fm_field_ignore_desc'].'</p>
+                    </div>
                     <div class="mode_checkbox">
-                        <h4><input type="checkbox" id="ctrl_' . $index . '[' . $viewObject['fieldID'] . ']" value="1" name="' . $this->strName . '[' . $index . '][set][overwrite]" ' . ($viewObject['set']['overwrite'] == '1' ? 'checked="checked"' : '') . '><label for="ctrl_' . $index . '[' . $viewObject['fieldID'] . ']">' . $GLOBALS['TL_LANG']['MSC']['fm_overwrite'] . '</label></h4>
+                        <h4><input type="checkbox" id="ctrl_' . $index . '[' . $viewObject['fieldID'] . '][overwrite]" value="1" name="' . $this->strName . '[' . $index . '][set][overwrite]" ' . ($viewObject['set']['overwrite'] == '1' ? 'checked="checked"' : '') . '><label for="ctrl_' . $index . '[' . $viewObject['fieldID'] . '][overwrite]">' . $GLOBALS['TL_LANG']['MSC']['fm_overwrite'] . '</label></h4>
                         <p class="tl_help tl_tip" title="">' . $GLOBALS['TL_LANG']['MSC']['fm_ignore'] . '</p>
                     </div>
                 </div>
 			</div>';
 		
 		return $template;
-	}	
+	}
 
+    /**
+     * @param $index
+     * @param $viewObject
+     * @return string
+     */
     private function setSimpleChoiceSettings($index, $viewObject)
     {
 
@@ -274,6 +311,11 @@ class ModeSettings extends Widget
         return $template;
     }
 
+    /**
+     * @param $index
+     * @param $viewObject
+     * @return string
+     */
     private function setMultiChoiceSettings($index, $viewObject)
     {
 
@@ -429,6 +471,10 @@ class ModeSettings extends Widget
         return $template;
     }
 
+    /**
+     * @param $viewObject
+     * @return array
+     */
     private function getDataFromTable($viewObject)
     {
         $o = array();
@@ -453,7 +499,9 @@ class ModeSettings extends Widget
         return $o;
     }
 
-
+    /*
+     *
+     */
     private function getOperator()
     {
         return array(

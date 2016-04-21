7611948d3570404a3605d83557d296366f972474
Alexander Naumov <naumov@tozen.de>
2016-04-21 15:21:57 +0200

    add new widget and js

--- config/config.php
+++ config/config.php
@@ -14,14 +14,13 @@
 // Path
 $GLOBALS['FM_AUTO_PATH'] = 'system/modules/fmodule/assets/';
 
-if( (version_compare(VERSION, '4.0', '>=') && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true ) )
-{
+if ((version_compare(VERSION, '4.0', '>=') && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true)) {
     $GLOBALS['FM_AUTO_PATH'] = 'bundles/fmodule/';
 }
 
 // Back End Modules
 $GLOBALS['BE_MOD']['system']['fmodule'] = array(
-    'icon' =>  $GLOBALS['FM_AUTO_PATH'].'icon.png',
+    'icon' => $GLOBALS['FM_AUTO_PATH'] . 'icon.png',
     'tables' => array(
         'tl_fmodules',
         'tl_fmodules_filters',
@@ -32,19 +31,19 @@ $GLOBALS['BE_MOD']['system']['fmodule'] = array(
 
 // Front End Modules
 array_insert($GLOBALS['FE_MOD'], 5, array(
-	'fmodule' => array(
-		'fmodule_fe_list' => 'ModuleListView',
-		'fmodule_fe_detail' => 'ModuleDetailView',
+    'fmodule' => array(
+        'fmodule_fe_list' => 'ModuleListView',
+        'fmodule_fe_detail' => 'ModuleDetailView',
         'fmodule_fe_formfilter' => 'ModuleFormFilter',
-		'fmodule_fe_registration' => 'ModuleFModuleRegistration'
-	)
+        'fmodule_fe_registration' => 'ModuleFModuleRegistration'
+    )
 ));
 
-// Widget
+// Widgets
 $GLOBALS['BE_FFL']['optionWizardExtended'] = 'OptionWizardExtended';
 $GLOBALS['BE_FFL']['modeSettings'] = 'ModeSettings';
 $GLOBALS['BE_FFL']['filterFields'] = 'FilterFields';
-
+$GLOBALS['BE_FFL']['keyValueWizardCustom'] = 'KeyValueWizardCustom';
 
 // Files
 if (TL_MODE == 'BE') {
@@ -95,10 +94,87 @@ $GLOBALS['TL_MOOTOOLS'][] =
                 }
 
                 return false;
-
             }
         }
-    </script>";
+    </script>
+    <script>
+    	if(Backend)
+    	{
+			Backend.keyValueWizardCustom = function(el, command, id) {
+			var table = $(id),
+				tbody = table.getElement('tbody'),
+				parent = $(el).getParent('tr'),
+				rows = tbody.getChildren(),
+				tabindex = tbody.get('data-tabindex'),
+				input, childs, i, j;
+
+			Backend.getScrollOffset();
+
+			switch (command) {
+					case 'copy':
+					var tr = new Element('tr');
+					childs = parent.getChildren();
+					for (i=0; i<childs.length; i++) {
+						var next = childs[i].clone(true).inject(tr, 'bottom');
+						if (input = childs[i].getFirst('input')) {
+							next.getFirst().value = input.value;
+						}
+						if (select = childs[i].getFirst('select')) {
+							next.getFirst('select').value = select.value;
+						}
+					}
+					tr.inject(parent, 'after');
+					$$(tr.getElement('.chzn-container')).destroy();
+					$$(tr.getElement('.tl_select_column')).destroy();
+					new Chosen(tr.getElement('select.tl_chosen'));
+					Stylect.convertSelects();
+
+					break;
+				case 'up':
+					if (tr = parent.getPrevious('tr')) {
+						parent.inject(tr, 'before');
+					} else {
+						parent.inject(tbody, 'bottom');
+					}
+					break;
+				case 'down':
+					if (tr = parent.getNext('tr')) {
+						parent.inject(tr, 'after');
+					} else {
+						parent.inject(tbody, 'top');
+					}
+					break;
+				case 'delete':
+					if (rows.length > 1) {
+						parent.destroy();
+					}
+					break;
+			}
+
+			rows = tbody.getChildren();
+
+			for (i=0; i<rows.length; i++) {
+				childs = rows[i].getChildren();
+				for (j=0; j<childs.length; j++) {
+					if (input = childs[j].getFirst('input')) {
+						input.set('tabindex', tabindex++);
+						input.name = input.name.replace(/\[[0-9]+]/g, '[' + i + ']')
+					}
+					if (input = childs[j].getFirst('select')) {
+						input.set('tabindex', tabindex++);
+						input.name = input.name.replace(/\[[0-9]+]/g, '[' + i + ']')
+					}
+				}
+			}
+
+			new Sortables(tbody, {
+				constrain: true,
+				opacity: 0.6,
+				handle: '.drag-handle'
+			});
+		}
+    }
+	</script>";
 
 // Permissions
 $GLOBALS['TL_PERMISSIONS'][] = 'fmodules';
@@ -113,8 +189,8 @@ $GLOBALS['TL_PERMISSIONS'][] = 'fmodulesfiltersp';
 // ProSearch
 $GLOBALS['PS_SEARCHABLE_MODULES']['fmodule'] = array(
     'tables' => array('tl_fmodules', 'tl_fmodules_filters'),
-    'searchIn' => array('name','tablename', 'info', 'title', 'type', 'fieldID'),
-    'title' => array('name','title'),
+    'searchIn' => array('name', 'tablename', 'info', 'title', 'type', 'fieldID'),
+    'title' => array('name', 'title'),
     'setCustomIcon' => array(array('ProSearchApi', 'setCustomIcon')),
     'setCustomShortcut' => array(array('ProSearchApi', 'setCustomShortcut'))
 );

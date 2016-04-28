53b724d02c71578707659ed363846640c9014cc2
Alexander Naumov <naumov@tozen.de>
2016-04-28 14:34:49 +0200

    add new fields for url translation

--- dca/tl_page.php
+++ dca/tl_page.php
@@ -11,13 +11,58 @@
  * @copyright 2016 Alexander Naumov
  */
 
-$GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace('includeLayout;', 'includeLayout;{fmodule_taxonomy_legend:hide},page_taxonomy;', $GLOBALS['TL_DCA']['tl_page']['palettes']['regular']);
+$GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace('includeLayout;', 'includeLayout;{fmodule_taxonomy_legend:hide},page_taxonomy;{fmodule_translateUrl},addTranslateUrl;', $GLOBALS['TL_DCA']['tl_page']['palettes']['regular']);
+$GLOBALS['TL_DCA']['tl_page']['palettes']['__selector__'][] = 'addTranslateUrl';
+$GLOBALS['TL_DCA']['tl_page']['subpalettes']['addTranslateUrl'] = 'translateUrl';
 
+// page_taxonomy
 $GLOBALS['TL_DCA']['tl_page']['fields']['page_taxonomy'] = array(
-
     'label' => &$GLOBALS['TL_LANG']['tl_page']['page_taxonomy'],
     'inputType' => 'modeSettings',
     'eval' => array('submitOnChange' => true),
     'sql' => "blob NULL"
-
 );
+
+// translateUrl
+$GLOBALS['TL_DCA']['tl_page']['fields']['translateUrl'] = array(
+    'label' => &$GLOBALS['TL_LANG']['tl_page']['translateUrl'],
+    'inputType' => 'select',
+    'options_callback' => array('tl_page_extend', 'getModules'),
+    'eval' => array('includeBlankOption' => true, 'blankOptionLabel' => '-'),
+    'sql' => "varchar(255) NOT NULL default ''"
+);
+
+// addTranslateUrl
+$GLOBALS['TL_DCA']['tl_page']['fields']['addTranslateUrl'] = array(
+    'label' => &$GLOBALS['TL_LANG']['tl_page']['fields']['addTranslateUrl'],
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'eval' => array('submitOnChange' => true),
+    'sql' => "char(1) NOT NULL default ''"
+);
+
+/**
+ * Class tl_page_extend
+ */
+class tl_page_extend extends tl_page
+{
+    /**
+     * @return array
+     */
+    public function getModules()
+    {
+        $modulesDB = $this->Database->prepare('SELECT * FROM tl_fmodules')->execute();
+        $arrModules = array();
+        if($modulesDB->count())
+        {
+            while($modulesDB->next())
+            {
+                if($modulesDB->tablename)
+                {
+                    $arrModules[$modulesDB->tablename] = $modulesDB->name;
+                }
+            }
+        }
+        return $arrModules;
+    }
+}

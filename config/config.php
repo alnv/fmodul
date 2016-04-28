b95bc41b5dc1e68fbbd92ab5e4e2680f488439a7
Alexander Naumov <naumov@tozen.de>
2016-04-28 16:40:44 +0200

    rename FModuleTranslation file

--- config/config.php
+++ config/config.php
@@ -61,7 +61,7 @@ $GLOBALS['TL_HOOKS']['loadDataContainer'][] = array('FModule', 'createUserGroupD
 $GLOBALS['TL_HOOKS']['autoComplete'][] = array('FModule', 'getAutoCompleteAjax');
 $GLOBALS['TL_HOOKS']['removeOldFeeds'][] = array('FModule', 'purgeOldFeeds');
 $GLOBALS['TL_HOOKS']['generateXmlFiles'][] = array('FModule', 'generateFeeds');
-$GLOBALS['TL_HOOKS']['translateUrlParameters'][] = array('FModuleLanguage', 'translateUrlParameters');
+$GLOBALS['TL_HOOKS']['translateUrlParameters'][] = array('FModuleTranslation', 'translateUrlParameters');
 
 // InsertTags
 $GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('FModuleInsertTags', 'setHooks');
---
+++ src/Resources/contao/classes/FModuleTranslation.php
@@ -0,0 +1,57 @@
+<?php namespace FModule;
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   F Modul
+ * @author    Alexander Naumov http://www.alexandernaumov.de
+ * @license   commercial
+ * @copyright 2016 Alexander Naumov
+ */
+
+use Contao\Frontend;
+
+/**
+ * Class FModuleTranslation
+ * @package FModule
+ */
+class FModuleTranslation extends Frontend
+{
+
+    /**
+     * @var string
+     */
+    protected $strTable = '';
+
+    /**
+     * @param $arrGet
+     * @param $strLanguage
+     * @return mixed
+     */
+    public function translateUrlParameters($arrGet, $strLanguage)
+    {
+        global $objPage;
+        if (!\Config::get('useAutoItem')) return $arrGet;
+        $alias = \Input::get('auto_item');
+        if (!$alias) return $arrGet;
+        if (isset($objPage->addTranslateUrl) && $objPage->addTranslateUrl == '1') {
+            $this->strTable = $objPage->translateUrl;
+        }
+        if (!$this->strTable) return $arrGet;
+        $table = $this->strTable;
+        $tableData = $this->strTable . '_data';
+        $currentItemDB = $this->Database->prepare('SELECT '.$tableData.'.*, '.$table.'.fallback, '.$table.'.language FROM '.$tableData.' LEFT OUTER JOIN '.$table.' ON '.$tableData.'.pid = '.$table.'.id WHERE '.$tableData.'.alias = ? OR '.$tableData.'.id = ?')->limit(1)->execute($alias, (int)$alias);
+        if ($currentItemDB->numRows)
+        {
+            $fallback = !$currentItemDB->fallback ? $currentItemDB->mainLanguage : $currentItemDB->alias;
+            $translationDB = $this->Database->prepare('SELECT '.$tableData.'.alias, '.$tableData.'.id, '.$tableData.'.mainLanguage, '.$table.'.language FROM '.$tableData.' LEFT OUTER JOIN '.$table.' ON '.$tableData.'.pid = '.$table.'.id WHERE '.$table.'.language = ? AND ('.$tableData.'.alias = ? OR '.$tableData.'.mainLanguage = ?)')->execute($strLanguage, $fallback, $fallback);
+            if ($translationDB->numRows) {
+                $arrGet['url']['items'] = $translationDB->alias ? $translationDB->alias : $translationDB->id;
+            }
+        }
+        return $arrGet;
+    }
+
+}

e64105cbf3329ca81fbbf9bc46f4cb0f994a2d3d
Alexander Naumov <naumov@tozen.de>
2016-05-23 09:36:33 +0200

    add language file for settings

---
+++ languages/en/tl_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['fmodule_legend'] = 'F Module-Settings';
+
+// googleApiKey
+$GLOBALS['TL_LANG']['tl_settings']['googleApiKey'][0] = 'Google maps api-key';
+$GLOBALS['TL_LANG']['tl_settings']['googleApiKey'][1] = 'Please enter your google api-key.';

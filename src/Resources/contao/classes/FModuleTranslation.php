af0619f750edf37b53ea2e3bd892a16f27a08225
Alexander Naumov <me@alexandernaumov.de>
2016-08-22 09:12:53 +0200

    implement change language v3 compatibility

--- src/Resources/contao/classes/FModuleTranslation.php
+++ src/Resources/contao/classes/FModuleTranslation.php
@@ -1,23 +1,22 @@
 <?php namespace FModule;
 
-/**
- * Contao Open Source CMS
- *
- * Copyright (c) 2005-2016 Leo Feyer
- *
- * @package   F Modul
- * @author    Alexander Naumov http://www.alexandernaumov.de
- * @license   commercial
- * @copyright 2016 Alexander Naumov
- */
+    /**
+     * Contao Open Source CMS
+     *
+     * Copyright (c) 2005-2016 Leo Feyer
+     *
+     * @package   F Modul
+     * @author    Alexander Naumov http://www.alexandernaumov.de
+     * @license   commercial
+     * @copyright 2016 Alexander Naumov
+     */
 
-use Contao\Frontend;
 
 /**
  * Class FModuleTranslation
  * @package FModule
  */
-class FModuleTranslation extends Frontend
+class FModuleTranslation extends \Frontend
 {
 
     /**
@@ -26,11 +25,57 @@ class FModuleTranslation extends Frontend
     protected $strTable = '';
 
     /**
+     * @param \Terminal42\ChangeLanguage\Event\ChangelanguageNavigationEvent $event
+     * @return null
+     */
+    public function translateUrlParameters(\Terminal42\ChangeLanguage\Event\ChangelanguageNavigationEvent $event)
+    {
+
+        global $objPage;
+        $targetRoot = $event->getNavigationItem()->getRootPage();
+        $strLanguage = $targetRoot->rootLanguage ? $targetRoot->rootLanguage : $targetRoot->language; // The target language
+
+        // Find your current and new alias from the current URL
+        if (!\Config::get('useAutoItem')) return null;
+
+        $varAlias = \Input::get('auto_item');
+
+        if (isset($objPage->addTranslateUrl) && $objPage->addTranslateUrl == '1') {
+            $this->strTable = $objPage->translateUrl;
+        }
+
+        if (!$this->strTable) return null;
+
+        $table = $this->strTable;
+        $tableData = $this->strTable . '_data';
+
+        // get current item
+        $objItem = $this->Database->prepare('SELECT ' . $tableData . '.*, ' . $table . '.fallback, ' . $table . '.language FROM ' . $tableData . ' LEFT OUTER JOIN ' . $table . ' ON ' . $tableData . '.pid = ' . $table . '.id WHERE ' . $tableData . '.alias = ? OR ' . $tableData . '.id = ?')->limit(1)->execute($varAlias, (int)$varAlias);
+
+        $newAlias = '';
+
+        if ($objItem->numRows) {
+            // get all items with the same fallback item
+            $fallback = !$objItem->fallback ? $objItem->mainLanguage : $objItem->id;
+            // select alias
+            $objTranslation = $this->Database->prepare('SELECT ' . $tableData . '.alias, ' . $tableData . '.id, ' . $tableData . '.mainLanguage, ' . $table . '.language FROM ' . $tableData . ' LEFT OUTER JOIN ' . $table . ' ON ' . $tableData . '.pid = ' . $table . '.id WHERE ' . $table . '.language = ? AND (' . $tableData . '.id = ? OR ' . $tableData . '.mainLanguage = ?)')->execute($strLanguage, $fallback, $fallback);
+
+            if ($objTranslation->numRows) {
+                $newAlias = $objTranslation->alias ? $objTranslation->alias : $objTranslation->id;
+            }
+        }
+
+
+        // Pass the new alias to ChangeLanguage
+        $event->getUrlParameterBag()->setUrlAttribute('items', $newAlias);
+    }
+
+    /**
      * @param $arrGet
      * @param $strLanguage
      * @return mixed
      */
-    public function translateUrlParameters($arrGet, $strLanguage)
+    public function translateUrlParametersBackwardsCompatibility($arrGet, $strLanguage)
     {
         global $objPage;
 
@@ -49,14 +94,13 @@ class FModuleTranslation extends Frontend
         $tableData = $this->strTable . '_data';
 
         // get current item
-        $currentItemDB = $this->Database->prepare('SELECT '.$tableData.'.*, '.$table.'.fallback, '.$table.'.language FROM '.$tableData.' LEFT OUTER JOIN '.$table.' ON '.$tableData.'.pid = '.$table.'.id WHERE '.$tableData.'.alias = ? OR '.$tableData.'.id = ?')->limit(1)->execute($alias, (int)$alias);
+        $currentItemDB = $this->Database->prepare('SELECT ' . $tableData . '.*, ' . $table . '.fallback, ' . $table . '.language FROM ' . $tableData . ' LEFT OUTER JOIN ' . $table . ' ON ' . $tableData . '.pid = ' . $table . '.id WHERE ' . $tableData . '.alias = ? OR ' . $tableData . '.id = ?')->limit(1)->execute($alias, (int)$alias);
 
-        if ($currentItemDB->numRows)
-        {
+        if ($currentItemDB->numRows) {
             // get all items with the same fallback item
             $fallback = !$currentItemDB->fallback ? $currentItemDB->mainLanguage : $currentItemDB->id;
             // select alias
-            $translationDB = $this->Database->prepare('SELECT '.$tableData.'.alias, '.$tableData.'.id, '.$tableData.'.mainLanguage, '.$table.'.language FROM '.$tableData.' LEFT OUTER JOIN '.$table.' ON '.$tableData.'.pid = '.$table.'.id WHERE '.$table.'.language = ? AND ('.$tableData.'.id = ? OR '.$tableData.'.mainLanguage = ?)')->execute($strLanguage, $fallback, $fallback);
+            $translationDB = $this->Database->prepare('SELECT ' . $tableData . '.alias, ' . $tableData . '.id, ' . $tableData . '.mainLanguage, ' . $table . '.language FROM ' . $tableData . ' LEFT OUTER JOIN ' . $table . ' ON ' . $tableData . '.pid = ' . $table . '.id WHERE ' . $table . '.language = ? AND (' . $tableData . '.id = ? OR ' . $tableData . '.mainLanguage = ?)')->execute($strLanguage, $fallback, $fallback);
             if ($translationDB->numRows) {
                 $arrGet['url']['items'] = $translationDB->alias ? $translationDB->alias : $translationDB->id;
             }

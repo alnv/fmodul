93389e2ea4ec316320292ca6ad24360902ff6a7c
Alexander Naumov <me@alexandernaumov.de>
2016-11-04 10:16:07 +0100

    add server key for g maps

--- dca/tl_settings.php
+++ dca/tl_settings.php
@@ -12,11 +12,18 @@
  */
 
 //
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('disableAlias;', 'disableAlias;{fmodule_legend:hide},googleApiKey,taxonomyDisable;', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('disableAlias;', 'disableAlias;{fmodule_legend:hide},googleApiKey,googleServerKey,taxonomyDisable;', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 // googleApiKey
 $GLOBALS['TL_DCA']['tl_settings']['fields']['googleApiKey'] = array(
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['googleApiKey'],
+    'inputType' => 'text',
+    'eval' => array('tl_class' => 'w50')
+);
+
+// googleApiKey
+$GLOBALS['TL_DCA']['tl_settings']['fields']['googleServerKey'] = array(
+    'label' => &$GLOBALS['TL_LANG']['tl_settings']['googleServerKey'],
     'inputType' => 'text',
     'eval' => array('tl_class' => 'w50')
 );

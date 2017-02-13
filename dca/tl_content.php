e182cdc6d66fbd71921307e9de1456727de8eb86
Alexander Naumov <me@alexandernaumov.de>
2017-02-13 14:22:13 +0100

    fix problem with save and new operation

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -11,95 +11,105 @@
  * @copyright 2016 Alexander Naumov
  */
 
-//
-$modules = array();
-$database = \Contao\Database::getInstance();
-if($database->tableExists('tl_fmodules') && empty($modules))
-{
-	$moduleDB = $database->prepare('SELECT * FROM tl_fmodules')->execute();
-	if($moduleDB->count())
-	{
-		while($moduleDB->next())
-		{
-			if($moduleDB->tablename)
-			{
-				$modules[] = substr($moduleDB->tablename, 3, strlen($moduleDB->tablename));
+$arrModules = [];
+$objDatabase = \Database::getInstance();
+
+if ( $objDatabase->tableExists('tl_fmodules') && empty( $arrModules ) )  {
+
+	$objModule = $objDatabase->prepare('SELECT * FROM tl_fmodules')->execute();
+
+	if ( $objModule->count() ) {
+
+		while( $objModule->next() ) {
+
+			if ( $objModule->tablename ) {
+
+				$arrModules[] = substr( $objModule->tablename, 3, strlen( $objModule->tablename ) );
 			}
 		}
 	}
 }
 
-//
-$GLOBALS['TL_DCA']['tl_content']['fields']['fview'] = array(
-	'label' => array("View", ""),
+$GLOBALS['TL_DCA']['tl_content']['fields']['fview'] = [
+
+	'label' => [ "View Mode", "F Module intern view mode." ],
 	'sql' => "varchar(50) NOT NULL default ''"
-);
+];
 
-//
-$view = \Contao\Input::get('view');
+$strViewMode = \Input::get('view');
 
-//
-foreach($modules as $tablename){
+foreach( $arrModules as $strTablename ){
 
-	if (\Contao\Input::get('do') == $tablename)
-	{
-		$GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'fm_'.$tablename.'_data';
-		$GLOBALS['TL_DCA']['tl_content']['list']['sorting']['filter'][] = array('fview = ?', $view);
+	if ( \Input::get('do') == $strTablename ) {
+
+		$GLOBALS['TL_DCA']['tl_content']['config']['ptable'] = 'fm_' . $strTablename . '_data';
+		$GLOBALS['TL_DCA']['tl_content']['list']['sorting']['filter'][] = [ 'fview = ?', $strViewMode ];
 	}
 }
 
-//
-$GLOBALS['TL_DCA']['tl_content']['config']['onsubmit_callback'][] = array('tl_content_fmodule', 'addView');
-$GLOBALS['TL_DCA']['tl_content']['config']['oncopy_callback'][] = array('tl_content_fmodule', 'onCopyAddFView');
-$GLOBALS['TL_DCA']['tl_content']['config']['oncut_callback'][] = array('tl_content_fmodule', 'onCutAddFView');
+$GLOBALS['TL_DCA']['tl_content']['config']['onsubmit_callback'][] = [ 'tl_content_fmodule', 'addView' ];
+$GLOBALS['TL_DCA']['tl_content']['config']['oncut_callback'][] = [ 'tl_content_fmodule', 'onCutAddFView' ];
+$GLOBALS['TL_DCA']['tl_content']['config']['oncopy_callback'][] = [ 'tl_content_fmodule', 'onCopyAddFView' ];
+$GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'][] = [ 'tl_content_fmodule', 'redirectToView' ];
 
-/**
- * Class tl_content_extend
- */
+
 class tl_content_fmodule extends \Contao\Backend
 {
 
-	/**
-	 * @param \Contao\DataContainer $dca
-	 * @return bool
-	 */
-	public function addView(\Contao\DataContainer $dca)
-	{
-		$view = \Contao\Input::get('view');
-		$id = $dca->activeRecord->id;
 
-		if($view)
-		{
-			$this->Database->prepare("UPDATE tl_content SET fview = ? WHERE id = ? LIMIT 1")->execute($view, $id);
+	public function redirectToView( \DataContainer $dc ) {
+
+		$strPID = \Input::get('pid');
+		$strViewMode = \Input::get('view');
+
+		if ( !$strViewMode && $strPID ) {
+
+			$objContent = $this->Database->prepare( 'SELECT * FROM tl_content WHERE id = ?' )->limit(1)->execute( $strPID );
+
+			if ( $objContent->numRows && $objContent->fview ) {
+
+				$strViewMode = $objContent->fview;
+				$strUri = \Environment::get('requestUri');
+
+				\Controller::redirect( $strUri . '&view='. $strViewMode );
+			}
 		}
+	}
+
+
+	public function addView( \DataContainer $dca ) {
+
+		$strViewMode = \Contao\Input::get('view');
+		$strID = $dca->activeRecord->id;
+
+		if ( $strViewMode ) {
+
+			$this->Database->prepare("UPDATE tl_content SET fview = ? WHERE id = ?")->execute( $strViewMode, $strID );
+		}
+
 		return true;
 	}
 
-	/**
-	 * @param \Contao\DataContainer $dca
-	 */
-	public function onCutAddFView(\Contao\DataContainer $dca)
-	{
-		$view = \Contao\Input::get('view');
-		$id = \Contao\Input::get('id');
 
-		if($id && $view)
-		{
-			$this->Database->prepare('UPDATE tl_content SET fview = ? WHERE id = ? LIMIT 1')->execute($view, $id);
+	public function onCutAddFView( \DataContainer $dca ) {
+
+		$strViewMode = \Contao\Input::get('view');
+		$strID = \Contao\Input::get('id');
+
+		if ( $strID && $strViewMode ) {
+
+			$this->Database->prepare('UPDATE tl_content SET fview = ? WHERE id = ?')->execute( $strViewMode, $strID );
 		}
 	}
 
-	/**
-	 * @param $id
-	 * @param \Contao\DataContainer $dca
-	 */
-	public function onCopyAddFView($id, \Contao\DataContainer $dca)
-	{
-		$view = \Contao\Input::get('view');
 
-		if($id && $view)
-		{
-			$this->Database->prepare('UPDATE tl_content SET fview = ? WHERE id = ? LIMIT 1')->execute($view, $id);
+	public function onCopyAddFView( $strID, \DataContainer $dca ) {
+
+		$strViewMode = \Contao\Input::get('view');
+
+		if ( $strID && $strViewMode ) {
+
+			$this->Database->prepare('UPDATE tl_content SET fview = ? WHERE id = ?')->execute( $strViewMode, $strID );
 		}
 	}
 }

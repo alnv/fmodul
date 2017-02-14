1c0ce8c57314c86c0519a5539b5b4bf7ff7c5215
Alexander Naumov <me@alexandernaumov.de>
2017-02-14 15:32:24 +0100

    add order by widget

--- config/autoload.php
+++ config/autoload.php
@@ -4,17 +4,13 @@ ClassLoader::addNamespace('FModule');
 
 $pathToFiles = 'system/modules/fmodule/';
 
-if ((version_compare(VERSION, '4.0', '>=') && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true)) {
+if ( ( version_compare( VERSION, '4.0', '>=' ) && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true ) ) {
     
     $pathToFiles = 'vendor/fmodule/fmodule/';
 }
 
-/**
- * Register the classes
- */
-ClassLoader::addClasses(array
-(
-    // classes
+ClassLoader::addClasses([
+
     'FModule\Initialize' => $pathToFiles . 'src/Resources/contao/classes/Initialize.php',
     'FModule\DCACreator' => $pathToFiles . 'src/Resources/contao/classes/DCACreator.php',
     'FModule\DCAModuleSettings' => $pathToFiles . 'src/Resources/contao/classes/DCAModuleSettings.php',
@@ -33,58 +29,49 @@ ClassLoader::addClasses(array
     'FModule\CleanUrls' => $pathToFiles . 'src/Resources/contao/classes/CleanUrls.php',
     'FModule\GalleryGenerator' => $pathToFiles . 'src/Resources/contao/classes/GalleryGenerator.php',
 
-    // widgets
-    'FModule\OptionWizardExtended' => $pathToFiles . 'src/Resources/contao/widgets/OptionWizardExtended.php',
     'FModule\ModeSettings' => $pathToFiles . 'src/Resources/contao/widgets/ModeSettings.php',
     'FModule\FilterFields' => $pathToFiles . 'src/Resources/contao/widgets/FilterFields.php',
+    'FModule\OptionWizardExtended' => $pathToFiles . 'src/Resources/contao/widgets/OptionWizardExtended.php',
     'FModule\KeyValueWizardCustom' => $pathToFiles . 'src/Resources/contao/widgets/KeyValueWizardCustom.php',
+    'FModule\FModuleOrderByWizard' => $pathToFiles . 'src/Resources/contao/widgets/FModuleOrderByWizard.php',
 
-    // forms
     'FModule\FormTextFieldCustom' => $pathToFiles . 'src/Resources/contao/forms/FormTextFieldCustom.php',
 
-    // modules
     'FModule\ModuleDetailView' => $pathToFiles . 'src/Resources/contao/module/ModuleDetailView.php',
     'FModule\ModuleListView' => $pathToFiles . 'src/Resources/contao/module/ModuleListView.php',
     'FModule\ModuleFModuleRegistration' => $pathToFiles . 'src/Resources/contao/module/ModuleFModuleRegistration.php',
     'FModule\ModuleFormFilter' => $pathToFiles . 'src/Resources/contao/module/ModuleFormFilter.php',
     'FModule\ModuleFModuleTaxonomy' => $pathToFiles . 'src/Resources/contao/module/ModuleFModuleTaxonomy.php',
 
-    // models
     'FModule\ContentModelExtend' => $pathToFiles . 'src/Resources/contao/model/ContentModelExtend.php',
     'FModule\QueryModel' => $pathToFiles . 'src/Resources/contao/models/QueryModel.php',
     'FModule\HelperModel' => $pathToFiles . 'src/Resources/contao/models/HelperModel.php',
     'FModule\FModuleModel' => $pathToFiles . 'src/Resources/contao/models/FModuleModel.php',
     'FModule\DataModel' => $pathToFiles . 'src/Resources/contao/models/DataModel.php',
 
-    // api
     'FModule\AjaxApi' => $pathToFiles . 'src/Resources/contao/api/AjaxApi.php',
 
-    // proSearch
     'FModule\ProSearchApi' => $pathToFiles . 'src/Resources/contao/classes/ProSearchApi.php'
-));
+]);
 
 $pathToTemplates = $pathToFiles . 'templates';
-if ((version_compare(VERSION, '4.0', '>=') && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true)) {
+
+if ( ( version_compare( VERSION, '4.0', '>=' ) && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true ) ) {
+
     $pathToTemplates = $pathToFiles . 'src/Resources/contao/templates';
 }
 
-/**
- * Register the templates
- */
-TemplateLoader::addFiles(array
-(
-    // modules
+TemplateLoader::addFiles([
+
     'mod_fmodule_detail' => $pathToTemplates,
     'mod_fmodule_list' => $pathToTemplates,
     'mod_form_filter' => $pathToTemplates,
     'mod_fmodule_map' => $pathToTemplates . '/maps',
 
-    // fe modules
     'fm_form_filter' => $pathToTemplates . '/form',
     'fmodule_full' => $pathToTemplates . '/list',
     'fmodule_teaser' => $pathToTemplates . '/list',
 
-    // widgets
     'fm_widget_date_field' => $pathToTemplates . '/widgets',
     'fm_widget_multi_choice' => $pathToTemplates . '/widgets',
     'fm_widget_simple_choice' => $pathToTemplates . '/widgets',
@@ -93,26 +80,20 @@ TemplateLoader::addFiles(array
     'fm_widget_wrapper_field' => $pathToTemplates . '/widgets',
     'fm_widget_toggle_field' => $pathToTemplates . '/widgets',
 
-    // geo
     'fm_widget_geo_locator' => $pathToTemplates . '/widgets',
 
-    // maps
     'fm_map_field' => $pathToTemplates . '/maps',
     'fm_map_location' => $pathToTemplates . '/maps',
 
-    // registration
     'sign_default' => $pathToTemplates . '/registration',
     'sign_grouped' => $pathToTemplates . '/registration',
 
-    // fields
     'fm_field_textarea' => $pathToTemplates . '/fields',
     'fm_field_text' => $pathToTemplates . '/fields',
     'fm_field_table' => $pathToTemplates . '/fields',
     'fm_field_list' => $pathToTemplates . '/fields',
 
-    // insertTags
     'fm_view' => $pathToTemplates . '/inserttags',
 
-    // taxonomies
-    'mod_taxonomies' => $pathToTemplates . '/taxonomies',
-));
+    'mod_taxonomies' => $pathToTemplates . '/taxonomies'
+]);
--- config/config.php
+++ config/config.php
@@ -11,186 +11,82 @@
  * @copyright 2016 Alexander Naumov
  */
 
-// path
 $GLOBALS['FM_AUTO_PATH'] = 'system/modules/fmodule/assets/';
 
-if ((version_compare(VERSION, '4.0', '>=') && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true)) {
+if ( ( version_compare( VERSION, '4.0', '>=' ) && !$GLOBALS['FM_NO_COMPOSER'] && $GLOBALS['FM_NO_COMPOSER'] != true ) ) {
+
     $GLOBALS['FM_AUTO_PATH'] = 'bundles/fmodule/';
 }
 
-// back end modules
-$GLOBALS['BE_MOD']['system']['fmodule'] = array(
+
+$GLOBALS['BE_MOD']['system']['fmodule'] = [
+
     'icon' => $GLOBALS['FM_AUTO_PATH'] . 'icon.png',
     'name' => 'F Module',
-    'tables' => array(
+
+    'tables' => [
+
         'tl_fmodules',
         'tl_fmodules_filters',
         'tl_fmodules_feed',
         'tl_fmodules_license'
-    )
-);
-$GLOBALS['BE_MOD']['system']['taxonomy'] = array(
+    ]
+];
+$GLOBALS['BE_MOD']['system']['taxonomy'] = [
+
     'icon' => $GLOBALS['FM_AUTO_PATH'] . 'tag.png',
     'name' => 'Taxonomy',
-    'tables' => array('tl_taxonomies')
-);
+    'tables' => [ 'tl_taxonomies' ]
+];
 
-// font end modules
-array_insert($GLOBALS['FE_MOD'], 5, array(
-    'fmodule' => array(
+
+array_insert($GLOBALS['FE_MOD'], 5, [
+
+    'fmodule' => [
+
         'fmodule_fe_list' => 'ModuleListView',
         'fmodule_fe_detail' => 'ModuleDetailView',
         'fmodule_fe_formfilter' => 'ModuleFormFilter',
-        'fmodule_fe_registration' => 'ModuleFModuleRegistration',
-        'fmodule_fe_taxonomy' => 'ModuleFModuleTaxonomy'
-    )
-));
+        'fmodule_fe_taxonomy' => 'ModuleFModuleTaxonomy',
+        'fmodule_fe_registration' => 'ModuleFModuleRegistration'
+    ]
+]);
 
-// widgets
-$GLOBALS['BE_FFL']['optionWizardExtended'] = 'OptionWizardExtended';
 $GLOBALS['BE_FFL']['modeSettings'] = 'ModeSettings';
 $GLOBALS['BE_FFL']['filterFields'] = 'FilterFields';
+$GLOBALS['BE_FFL']['optionWizardExtended'] = 'OptionWizardExtended';
 $GLOBALS['BE_FFL']['keyValueWizardCustom'] = 'KeyValueWizardCustom';
+$GLOBALS['BE_FFL']['catalogOrderByWizard'] = 'FModuleOrderByWizard';
 
-// files
+
 if (TL_MODE == 'BE') {
 
     $GLOBALS['TL_CSS'][] = $GLOBALS['FM_AUTO_PATH'] . 'stylesheet.css';
 }
 
-// google Maps
 $GLOBALS['loadGoogleMapLibraries'] = false;
 
-// hooks
-$GLOBALS['TL_HOOKS']['initializeSystem'][] = array('Initialize', 'getClasses');
-$GLOBALS['TL_HOOKS']['postLogin'][] = array('FModule', 'setLanguage');
-$GLOBALS['TL_HOOKS']['getSearchablePages'][] = array('FModule', 'getSearchablePages');
-$GLOBALS['TL_HOOKS']['loadDataContainer'][] = array('FModule', 'createUserGroupDCA');
-$GLOBALS['TL_HOOKS']['autoComplete'][] = array('FModule', 'getAutoCompleteAjax');
-$GLOBALS['TL_HOOKS']['removeOldFeeds'][] = array('FModule', 'purgeOldFeeds');
-$GLOBALS['TL_HOOKS']['generateXmlFiles'][] = array('FModule', 'generateFeeds');
-$GLOBALS['TL_HOOKS']['getPageIdFromUrl'][] = array('CleanUrls', 'getPageIdFromUrlStr');
+$GLOBALS['TL_HOOKS']['postLogin'][] = [ 'FModule', 'setLanguage' ];
+$GLOBALS['TL_HOOKS']['removeOldFeeds'][] = [ 'FModule', 'purgeOldFeeds' ];
+$GLOBALS['TL_HOOKS']['generateXmlFiles'][] = [ 'FModule', 'generateFeeds' ];
+$GLOBALS['TL_HOOKS']['initializeSystem'][] = [ 'Initialize', 'getClasses' ];
+$GLOBALS['TL_HOOKS']['autoComplete'][] = [ 'FModule', 'getAutoCompleteAjax' ];
+$GLOBALS['TL_HOOKS']['loadDataContainer'][] = ['FModule', 'createUserGroupDCA' ];
+$GLOBALS['TL_HOOKS']['getSearchablePages'][] = [ 'FModule', 'getSearchablePages' ];
+$GLOBALS['TL_HOOKS']['getPageIdFromUrl'][] = [ 'CleanUrls', 'getPageIdFromUrlStr' ];
 
-// change language module hooks
-$GLOBALS['TL_HOOKS']['changelanguageNavigation'][] = array('FModuleTranslation', 'translateUrlParameters'); // v3
-$GLOBALS['TL_HOOKS']['translateUrlParameters'][] = array('FModuleTranslation', 'translateUrlParametersBackwardsCompatible'); // backwards compatibility
+$GLOBALS['TL_HOOKS']['changelanguageNavigation'][] = [ 'FModuleTranslation', 'translateUrlParameters' ];
+$GLOBALS['TL_HOOKS']['translateUrlParameters'][] = [ 'FModuleTranslation', 'translateUrlParametersBackwardsCompatible' ];
 
+$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = [ 'FModuleInsertTags', 'setHooks' ];
 
+if ( TL_MODE == 'BE' ) {
 
-// insertTags
-$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('FModuleInsertTags', 'setHooks');
+    $GLOBALS['TL_JAVASCRIPT']['catalogManagerBackendExtension'] = $GLOBALS['TL_CONFIG']['debugMode']
+        ? $GLOBALS['FM_AUTO_PATH'] . 'FModule.js'
+        : $GLOBALS['FM_AUTO_PATH'] . 'FModule.js';
+}
 
-// ajax
-$GLOBALS['TL_MOOTOOLS'][] =
-    "<script>
-        if(typeof AjaxRequest != 'undefined')
-        {
-            AjaxRequest.toggleFMField = function(el)
-            {
-                el.blur();
-                var image = $(el).getFirst('img');
-                var href = $(el).get('href');
-                var tempSrc = image.get('src');
-                var src = image.get('data-src');
-
-                var featured = (image.get('data-state') == 1);
-
-		        if (!featured) {
-                    image.src = src;
-                    image.set('data-src', tempSrc);
-                    image.set('data-state', 1);
-                    new Request({'url': href}).get({'rt': Contao.request_token});
-                } else {
-                    image.src = src;
-                    image.set('data-src', tempSrc);
-                    image.set('data-state', 0);
-                    new Request({'url': href}).get({'rt':Contao.request_token});
-                }
-
-                return false;
-            }
-        }
-    </script>
-    <script>
-    	if(typeof Backend != 'undefined')
-    	{
-			Backend.keyValueWizardCustom = function(el, command, id) {
-			var table = $(id),
-				tbody = table.getElement('tbody'),
-				parent = $(el).getParent('tr'),
-				rows = tbody.getChildren(),
-				tabindex = tbody.get('data-tabindex'),
-				input, childs, i, j;
-
-			Backend.getScrollOffset();
-
-			switch (command) {
-					case 'copy':
-					var tr = new Element('tr');
-					childs = parent.getChildren();
-					for (i=0; i<childs.length; i++) {
-						var next = childs[i].clone(true).inject(tr, 'bottom');
-						if (input = childs[i].getFirst('input')) {
-							next.getFirst().value = input.value;
-						}
-						if (select = childs[i].getFirst('select')) {
-							next.getFirst('select').value = select.value;
-						}
-					}
-					tr.inject(parent, 'after');
-					$$(tr.getElement('.chzn-container')).destroy();
-					$$(tr.getElement('.tl_select_column')).destroy();
-					new Chosen(tr.getElement('select.tl_chosen'));
-					Stylect.convertSelects();
-
-					break;
-				case 'up':
-					if (tr = parent.getPrevious('tr')) {
-						parent.inject(tr, 'before');
-					} else {
-						parent.inject(tbody, 'bottom');
-					}
-					break;
-				case 'down':
-					if (tr = parent.getNext('tr')) {
-						parent.inject(tr, 'after');
-					} else {
-						parent.inject(tbody, 'top');
-					}
-					break;
-				case 'delete':
-					if (rows.length > 1) {
-						parent.destroy();
-					}
-					break;
-			}
-
-			rows = tbody.getChildren();
-
-			for (i=0; i<rows.length; i++) {
-				childs = rows[i].getChildren();
-				for (j=0; j<childs.length; j++) {
-					if (input = childs[j].getFirst('input')) {
-						input.set('tabindex', tabindex++);
-						input.name = input.name.replace(/\[[0-9]+]/g, '[' + i + ']')
-					}
-					if (input = childs[j].getFirst('select')) {
-						input.set('tabindex', tabindex++);
-						input.name = input.name.replace(/\[[0-9]+]/g, '[' + i + ']')
-					}
-				}
-			}
-
-			new Sortables(tbody, {
-				constrain: true,
-				opacity: 0.6,
-				handle: '.drag-handle'
-			});
-		}
-    }
-	</script>";
-
-// permissions
 $GLOBALS['TL_PERMISSIONS'][] = 'fmodules';
 $GLOBALS['TL_PERMISSIONS'][] = 'fmodulesp';
 
@@ -203,26 +99,23 @@ $GLOBALS['TL_PERMISSIONS'][] = 'fmodulesfeedp';
 $GLOBALS['TL_PERMISSIONS'][] = 'fmodulesfilters';
 $GLOBALS['TL_PERMISSIONS'][] = 'fmodulesfiltersp';
 
-// proSearch
-$GLOBALS['PS_SEARCHABLE_MODULES']['fmodule'] = array(
-    'tables' => array('tl_fmodules', 'tl_fmodules_filters'),
-    'searchIn' => array('name', 'tablename', 'info', 'title', 'type', 'fieldID'),
-    'title' => array('name', 'title'),
-    'setCustomIcon' => array(array('ProSearchApi', 'setCustomIcon')),
-    'setCustomShortcut' => array(array('ProSearchApi', 'setCustomShortcut'))
-);
 
-// wrapper
+$GLOBALS['PS_SEARCHABLE_MODULES']['fmodule'] = [
+
+    'title' => [ 'name', 'title' ],
+    'tables' => [ 'tl_fmodules', 'tl_fmodules_filters' ],
+    'setCustomIcon' => [ [ 'ProSearchApi', 'setCustomIcon' ] ],
+    'setCustomShortcut' => [ [ 'ProSearchApi', 'setCustomShortcut' ] ],
+    'searchIn' => [ 'name', 'tablename', 'info', 'title', 'type', 'fieldID' ]
+];
+
 $GLOBALS['TL_WRAPPERS']['start'][] = 'legend_start';
 $GLOBALS['TL_WRAPPERS']['stop'][] = 'legend_end';
 
-// check license
+
 if (TL_MODE == 'FE') {
-    
-    /**
-     * @var array
-     */
-    $validSums = array(
+
+    $arrValidSums = array(
 
         'ace73f5761f137a394516b1c4e4e2d9f',
         'ed34fd458b6bc5445f449118fb4e538c',
@@ -278,7 +171,7 @@ if (TL_MODE == 'FE') {
 
     $strLicense = Contao\Config::get('fmodule_license');
 
-    if ( !isset( $strLicense ) || !in_array( md5( $strLicense ), $validSums, true ) ) {
+    if ( !isset( $strLicense ) || !in_array( md5( $strLicense ), $arrValidSums, true ) ) {
 
         $GLOBALS['TL_HEAD'][] = '<link title="F Module | Buy license" rel="license" href="http://fmodul.alexandernaumov.de/kaufen.html" />';
         $GLOBALS['TL_HEAD'][] = '<link title="F Module | Documentation" rel="help" href="http://fmodul.alexandernaumov.de/ressourcen.html" />';
